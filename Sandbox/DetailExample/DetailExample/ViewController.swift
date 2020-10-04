c5639aa81fd52d7a5f6deddedd69302bd900dbd7
Mingu Kang <minqu.kang@gmail.com>
2020-10-04 15:16:43 +0900

    New Project

---
+++ Sandbox/DetailExample/DetailExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetailExample
+//
+//  Created by Kang Mingu on 2020/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
