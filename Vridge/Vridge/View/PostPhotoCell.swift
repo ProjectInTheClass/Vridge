f9352e70bda5833529d9ac6278e4d3e39485d058
Mingu Kang <minqu.kang@gmail.com>
2020-10-05 16:53:51 +0900

    ignore all xcuserstate

--- Vridge/Vridge/View/PostPhotoCell.swift
+++ Vridge/Vridge/View/PostPhotoCell.swift
@@ -15,7 +15,7 @@ class PostPhotoCell: UICollectionViewCell {
         let iv = UIImageView()
         iv.contentMode = .scaleToFill
         iv.clipsToBounds = true
-        iv.tintColor = .lightGray
+        iv.tintColor = .gray
         iv.layer.borderColor = UIColor.vridgeGreen.cgColor
         iv.layer.borderWidth = 1.2
         return iv
