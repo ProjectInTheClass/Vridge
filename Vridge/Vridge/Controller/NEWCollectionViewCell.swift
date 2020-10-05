921d05a67d64d8f8231cd60cfda51a3db9502c95
Mingu Kang <minqu.kang@gmail.com>
2020-10-05 15:46:36 +0900

    NEW collectionviewCell

---
+++ Vridge/Vridge/Controller/NEWCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NEWCollectionViewCell.swift
+//  Vridge
+//
+//  Created by Kang Mingu on 2020/10/05.
+//
+
+import UIKit
+
+class NEWCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
